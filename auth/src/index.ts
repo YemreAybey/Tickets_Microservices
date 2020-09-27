1dab5a0c399b01d1a343e4813df2f6a5f74454aa
Emre Aybey <Emre.Aybey@adesso.com.tr>
2020-09-27 17:11:29 +0300

    added comment for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,14 @@
-import mongoose from "mongoose";
-import app from "./app";
+import mongoose from 'mongoose';
+import app from './app';
 
 const start = async () => {
+  console.log('comment for auth');
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
+    throw new Error('JWT_KEY must be defined');
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
+    throw new Error('MONGO_URI must be defined');
   }
 
   try {
@@ -21,7 +22,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log("listening on 3000 !!!!!");
+    console.log('listening on 3000 !!!!!');
   });
 };
 
