4c0ced0afbcd018b5b068cc4d8298d1e328816a3
Emre Aybey <Emre.Aybey@adesso.com.tr>
2020-09-27 19:32:30 +0300

    some deployment fix

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError, currentUser } from "@eatickets/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@eatickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    secure: false,
   })
 );
 
@@ -24,7 +24,7 @@ app.use(showTicketRouter);
 app.use(indexTicketRouter);
 app.use(updateTicketRouter);
 
-app.all("*", async () => {
+app.all('*', async () => {
   throw new NotFoundError();
 });
 app.use(errorHandler);
