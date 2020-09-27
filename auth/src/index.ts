887fae6697659510ab322748d5bcbb87e94712cd
Emre Aybey <Emre.Aybey@adesso.com.tr>
2020-09-27 18:45:36 +0300

    updated startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import app from './app';
 
 const start = async () => {
-  console.log('comment for auth');
+  console.log('comment for auth...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
